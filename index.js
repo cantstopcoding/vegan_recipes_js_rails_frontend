f5a157603993dc474e46871bbdc13e64fe834cc6
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-23 16:32:31 -0400

    get all recipes when dom is loaded

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("in index.js");
+document.addEventListener("DOMContentLoaded", () => {
+  getRecipes();
+});
+
+const getRecipes = () => {
+  fetch("http://localhost:3000/api/v1/recipes")
+    .then((response) => response.json())
+    .then((recipes) => {
+      recipes.data.forEach((recipe) => {
+        const recipeMarkup = `
+            <div data-id=${recipe.id}>
+                <img src=${recipe.attributes.image_url} height="200" width="250">
+                <h3>${recipe.attributes.title}</h3>
+                <p>${recipe.attributes.ingredients}</p>
+                <p>${recipe.attributes.instructions}</p>
+                <p>${recipe.attributes.category.name}</p>
+                <button data-id={recipe.id}>edit</button>
+            </div>
+            <br>
+            <br>
+        `;
+
+        document.querySelector("#recipe-container").innerHTML += recipeMarkup;
+      });
+    })
+    .catch((err) => console.log(err, "this is an error!!!"));
+};
