c831795bab671c7c28d3d856e5a6cf43f159ea22
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-27 14:18:04 -0400

    create renderRecipeCard method

--- src/recipe.js
+++ src/recipe.js
@@ -7,8 +7,22 @@ class Recipe {
       (this.image_url = recipeAttributes.image_url),
       (this.category = recipeAttributes.category);
     Recipe.all.push(this);
-    debugger;
   }
+
+  renderRecipeCard = () => {
+    return `
+      <div data-id=${this.id}>
+        <img src=${this.image_url} height="200" width="250">
+        <h3>${this.title}</h3>
+        <p>${this.ingredients}</p>
+        <p>${this.instructions}</p>
+        <p>${this.category.name}</p>
+        <button data-id=${this.id}>edit</button>
+      </div>
+      <br>
+      <br>
+    `;
+  };
 }
 
 Recipe.all = [];
